eae6059e8dc1599e690b82e831af6cdaaa7d46ea
Nicolae <nicolae.vasile@smoothstack.com>
2019-10-09 10:55:43 -0400

    created project

---
+++ MyNewProject/src/com/ss/servlet/DisasterServlet.java
@@ -0,0 +1,81 @@
+package com.ss.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.ss.dto.LoginInfo;
+
+/**
+ * Servlet implementation class DisasterServlet
+ */
+
+@WebServlet(urlPatterns= {"/login","/login/"})
+public class DisasterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DisasterServlet() {
+        super();
+    }
+
+    public void init() 
+    {
+    	System.out.println("*****************************");
+    	System.out.println("**  LoginServlet Starting  **");
+    	System.out.println("*****************************");
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		// create our list of valid logins
+		List<LoginInfo> logins = new ArrayList<LoginInfo>();
+		logins.add(new LoginInfo("user1", "pass1"));
+		logins.add(new LoginInfo("user2", "pass2"));
+		
+		Gson gson = new Gson();
+		
+		LoginInfo sentData = gson.fromJson(request.getReader(), LoginInfo.class);
+
+		//log("Data sent: " + sentData.getUsername() + " " + sentData.getPassword());
+		
+		boolean loggedIn = false;
+		for(LoginInfo login: logins) 
+		{
+			if(sentData.getUsername().equals(login.getUsername()) &&
+				sentData.getPassword().equals(login.getPassword())) 
+			{
+				loggedIn = true;
+			}
+		}
+		
+		if(loggedIn)
+		{
+			response.sendRedirect("success.jsp");
+		}
+		else 
+		{
+			response.sendRedirect("fail.jsp");
+		}
+	}
+}
